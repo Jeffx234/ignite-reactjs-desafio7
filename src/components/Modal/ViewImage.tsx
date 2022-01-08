e604b163de7e4627e6a4418d82548d5ec7f2de90
Jeffx234 <jefersonluisx@hotmail.com>
2022-01-08 19:10:27 -0300

    finalizando desafio

--- src/components/Modal/ViewImage.tsx
+++ src/components/Modal/ViewImage.tsx
@@ -19,5 +19,20 @@ export function ModalViewImage({
   onClose,
   imgUrl,
 }: ModalViewImageProps): JSX.Element {
+  return(
+    <Modal isOpen={isOpen} onClose={onClose}>
+      <ModalOverlay/>
+
+      <ModalContent bgCOlor="pGray.600">
+        <Image src={imgUrl} maxH={600} MAXw={900} />
+
+        <ModalFooter justifyContent="flex-start">
+          <Link href={imgUrl} target="_blank">
+            Abrir original
+          </Link>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
+  )
   // TODO MODAL WITH IMAGE AND EXTERNAL LINK
 }
